fb07f0766fd739dfc4a542b1393eeb68f4b66d24
jarkub <jared.kubly@gmail.com>
2015-02-08 16:18:44 -0700

    plot R files

---
+++ plot2.R
@@ -0,0 +1,23 @@
+library(lubridate)
+library(dplyr)
+if(!file.exists("./data")) {
+    dir.create("./data")
+}
+if(!file.exists("./data/household_power_consumption.txt")) {
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                  "./data/household_power_consumption.zip", mode="wb")
+    unzip("./data/household_power_consumption.zip", exdir="./data")
+}
+power <- read.table("./data/household_power_consumption.txt", 
+                    sep=";", 
+                    header=TRUE,
+                    colClasses=c(rep("character",2),rep("numeric",7)),
+                    na.strings="?")
+# 2007-02-01 and 2007-02-02 only
+power <- filter(power, Date == "1/2/2007" | Date == "2/2/2007")
+power$DateTime <- paste(power$Date, power$Time, sep=" ")
+power$DateTime <- dmy_hms(power$DateTime)
+
+png("./plot2.png")
+with(power, plot(DateTime, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)", cex.lab=0.8, cex.axis=0.8))
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,34 @@
+library(lubridate)
+library(dplyr)
+if(!file.exists("./data")) {
+    dir.create("./data")
+}
+if(!file.exists("./data/household_power_consumption.txt")) {
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                  "./data/household_power_consumption.zip", mode="wb")
+    unzip("./data/household_power_consumption.zip", exdir="./data")
+}
+power <- read.table("./data/household_power_consumption.txt", 
+                    sep=";", 
+                    header=TRUE,
+                    colClasses=c(rep("character",2),rep("numeric",7)),
+                    na.strings="?")
+# 2007-02-01 and 2007-02-02 only
+power <- filter(power, Date == "1/2/2007" | Date == "2/2/2007")
+power$DateTime <- paste(power$Date, power$Time, sep=" ")
+power$DateTime <- dmy_hms(power$DateTime)
+
+png("./plot4.png")
+par(mfrow=c(2,2))
+with(power, plot(DateTime, Global_active_power, type="l", xlab="", ylab="Global Active Power", cex.lab=0.8, cex.axis=0.8))
+
+with(power, plot(DateTime, Voltage, type="l", xlab="datetime", cex.lab=0.8, cex.axis=0.8))
+
+with(power, plot(DateTime, Sub_metering_1, type="n", xlab="", ylab="Energy sub metering", cex.lab=0.8, cex.axis=0.8))
+with(power, lines(DateTime, Sub_metering_1))
+with(power, lines(DateTime, Sub_metering_2, col="red"))
+with(power, lines(DateTime, Sub_metering_3, col="blue"))
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lty=1, cex=0.8, bty="n")
+
+with(power, plot(DateTime, Global_reactive_power, type="l", xlab="datetime", cex.lab=0.8, cex.axis=0.8))
+dev.off()
